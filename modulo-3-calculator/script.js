c5d4368c77f4a4e10fee032e787c662dee8d55ad
wlricardo <wlricardo@gmail.com>
2024-09-02 21:42:36 -0300

    closes #15, closes #16 - Efetuando as 4 operações matemáticas básicas

--- modulo-3-calculator/script.js
+++ modulo-3-calculator/script.js
@@ -1,7 +1,21 @@
-function calc() {
+function calc(op) {
   var num1 = parseFloat(document.getElementById("first-value").value);
   var num2 = parseFloat(document.getElementById("second-value").value);
+  var result = 0;
 
-  var result = num1 + num2;
+  switch (op) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+  }
+
   document.getElementById("result").value = result;
 }
